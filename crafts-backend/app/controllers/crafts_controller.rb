cf056d55d221f8f75b8e9005bce001621a8979ce
annie_truex <artruex@gmail.com>
2020-05-18 16:13:51 -0700

    setup cors and routes namespace

--- crafts-backend/app/controllers/crafts_controller.rb
+++
@@ -1,2 +0,0 @@
-class CraftsController < ApplicationController
-end
--- crafts-backend/config/routes.rb
+++ crafts-backend/config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  resources :materials
-  resources :crafts
+
+  namespace :api do
+    namespace :v1 do
+    resources :materials
+    resources :crafts
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

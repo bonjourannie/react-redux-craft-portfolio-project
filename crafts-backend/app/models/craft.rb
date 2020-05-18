472f3f39204d8d6519417919493dbb786b11fb62
annie_truex <artruex@gmail.com>
2020-05-18 15:25:28 -0700

    add tables and relationships to backend

---
+++ crafts-backend/app/models/craft.rb
@@ -0,0 +1,5 @@
+class Craft < ApplicationRecord
+    has_many :materials 
+    
+    validates :name, presence: true
+end
---
+++ crafts-backend/db/migrate/20200518221354_create_materials.rb
@@ -0,0 +1,12 @@
+class CreateMaterials < ActiveRecord::Migration[6.0]
+  def change
+    create_table :materials do |t|
+      t.string :name
+      t.integer :craft_id
+      t.string :description
+      
+
+      t.timestamps
+    end
+  end
+end

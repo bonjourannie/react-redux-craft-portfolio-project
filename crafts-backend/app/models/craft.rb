ea0a52ae1bd9fb518eb51677ddbd7200e66b70d6
annie_truex <artruex@gmail.com>
2020-05-18 19:26:10 -0700

    add crafts controller

--- crafts-backend/app/models/craft.rb
+++ crafts-backend/app/models/craft.rb
@@ -1,5 +1,11 @@
 class Craft < ApplicationRecord
     has_many :materials 
+    validates :name, presence: true
     
     validates :name, presence: true
+
+    # def update_materials(material)
+    #     self.materials.push
+    # end
+
 end
